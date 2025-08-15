4e01e55d4072c51bb9bc1e8a6823b7fce8ebbfeb
“Earnars123” <earnarsproject123@gmail.com>
2025-08-15 16:36:36 +0100

    worked on the mobile navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -118,23 +118,13 @@ const Header = () => {
       <div className='baclo' onClick={handleCheckboxChange}></div>
     <ul className="menuBox24">
       <li><div className='gFerL'><FontAwesomeIcon icon={faXmark}  className=" " style={{fontSize:"26px",color:"#2f3a5b"}} onClick={handleCheckboxChange} /></div></li>
-      <li><div className='reff aaFF'><a href="/login"><button className='RegRef' style={{background:"#2e756d"}}>Sign In</button></a><a href="/register"><button className='RegRef'>Register</button></a></div></li>
+     
             <li><a className="menuItem" href="/about">About Us</a></li>
-       <li><a className="menuItem met" onClick={()=>setDropdown(!dropdown)} >Services <span style={{color:"#fff",marginLeft:"5px",fontSize:"18px"}}>{dropdown ? <FontAwesomeIcon icon={faCircleChevronUp}  className=" " style={{fontSize:"20px",color:"#c5c5c5"}}  />: <FontAwesomeIcon icon={faCircleChevronDown}  className=" " style={{fontSize:"20px",color:"#c5c5c5"}}  />}</span></a>
-       {dropdown ? 
-          <div>
-             <div className='mns41'><div className='mns4'><FontAwesomeIcon icon={faArrowRight}  className=" " style={{fontSize:"20px",color:"#c5c5c5",marginRight:"8px"}}  />Web Development</div></div>
-             <div className='mns41'><div className='mns4'><FontAwesomeIcon icon={faArrowRight}  className=" " style={{fontSize:"20px",color:"#c5c5c5",marginRight:"8px"}}  />Mobile App Development</div></div>
-              <div className='mns41'><div className='mns4'><FontAwesomeIcon icon={faArrowRight}  className=" " style={{fontSize:"20px",color:"#c5c5c5",marginRight:"8px"}}  />UI/UX design</div></div>
-                <div className='mns41'><div className='mns4'><FontAwesomeIcon icon={faArrowRight}  className=" " style={{fontSize:"20px",color:"#c5c5c5",marginRight:"8px"}}  />Graphic Design</div></div>
-                <div className='mns41'><div className='mns4'><FontAwesomeIcon icon={faArrowRight}  className=" " style={{fontSize:"20px",color:"#c5c5c5",marginRight:"8px"}}  />Front_End Development</div></div>
-                <div className='mns41'><div className='mns4'><FontAwesomeIcon icon={faArrowRight}  className=" " style={{fontSize:"20px",color:"#c5c5c5",marginRight:"8px"}}  />Backend Development</div></div>
-          </div>
-          :""}
-       </li>
+             <li><a className="menuItem" href="/services">Services</a></li>
+       
               <li><a className="menuItem" href="/career">Career</a></li>
-              <li><a className="menuItem"  onClick={handleChange2}>Industries</a></li>
-              <li><a className="menuItem" href="/">How It Works</a></li>
+              
+          
       <li><a className="menuItem" href="/blogs" style={{marginBottom:"10px"}}>Blog</a></li>
 
      
